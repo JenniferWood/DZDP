6aff70db618df3ebe838c65040cbc5455ff2cab1
Jean Wood <beusedtobeingking@gmail.com>
2017-12-27 09:14:54 +0800

    Add error handler for 'process'

--- utils/process.py
+++ utils/process.py
@@ -1,19 +1,28 @@
+from pymongo import errors
 from db import mongo
 
 if __name__ == "__main__":
     obj = mongo.MyMongoDb("dzdp")
 
-    count = 0
-    for item in obj.get_all("wishlist_f"):
-        count += 1
-        if count % 10 == 0:
+    count, last_count = 0, 0
+    while True:
+        try:
+            for item in obj.get_all("wishlist_f"):
+                count += 1
+                if count % 10 == 0:
+                    print count
+                member_id = item["member-id"]
+                wish_list = item["wishlist"]
+                for shop in wish_list:
+                    json = {"member-id": member_id, "shop-id": shop}
+                    if obj.exists_by_key("wishlist", json):
+                        continue
+                    obj.insert("wishlist", **json)
+                obj.remove("wishlist_f", **item)
+        except errors.CursorNotFound, ex:
+            print ex
+        finally:
             print count
-        member_id = item["member-id"]
-        wish_list = item["wishlist"]
-        for shop in wish_list:
-            json = {"member-id": member_id, "shop-id": shop}
-            if obj.exists_by_key("wishlist", json):
-                continue
-            obj.insert("wishlist", **json)
-        obj.remove("wishlist_f", **item)
-    print count
+            if count == last_count:
+                break
+            last_count = count
